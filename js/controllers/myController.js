7c42bcc69f0585695e4d56c44d80ada0b73df3ee
Javitron <javier.lisbona@es.ibm.com>
2017-09-19 14:30:24 +0200

    clean code
    clean code

--- js/controllers/myController.js
+++ js/controllers/myController.js
@@ -1,12 +1,1 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww24940\viewh8400\viewkind0
-\deftab720
-
-\itap1\trowd \taflags0 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
-\clvertalc \clshdrawnil \clwWidth15680\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\sl280\partightenfactor0
-
-\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
-app.controller('myController',['$scope', function($scope)\{<br/>$scope.name= \'93Hello Unicaja Team: This is my First Angular App";<br/>$scope.version = "v1.0.1";<br/>$scope.author = "Created by &lt;Javi&gt;";<br/>$scope.release ="Released on &lt;25/09/2017&gt;";<br/>$scope.message ="&lt; Hello Unicaja Team!! &gt;";<br>\}]);<br/>\cell \lastrow\row}
+app.controller('myController',['$scope', function($scope){<br/>$scope.name= “Hello Unicaja Team: This is my First Angular App";<br/>$scope.version = "v1.0.1";<br/>$scope.author = "Created by &lt;Javi&gt;";<br/>$scope.release ="Released on &lt;25/09/2017&gt;";<br/>$scope.message ="&lt; Hello Unicaja Team!! &gt;";<br>}]);<br/>
